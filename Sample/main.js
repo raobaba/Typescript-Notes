b2ffbf3ca498718d7bc0a145ee144b7feef2891b
raobaba <raorajan9576@gmail.com>
2023-05-10 12:45:30 +0530

    Assigning type

--- Sample/main.js
+++ Sample/main.js
@@ -1,16 +1,21 @@
 // var input1:Element | null = document.querySelector('#number1') as HTMLInputElement;
 // var input2:Element | null = document.querySelector('#number2') as HTMLIFrameElement;
 // var button = document.querySelector('#button');
-// const add = (number1:number,number2:number)=>{
+// const addition = (number1:number,number2:number)=>{
 //     return number1+number2
 // }
 // button?.addEventListener('click',()=> {
 //    console.log(add(+input1.value,+input2.value));
 // });
-function add(number1, number2) {
+var add = function (number1, number2) {
     return number1 + number2;
-}
-var n1 = '1.3';
-var n2 = 4;
+};
+//It is ok if you are assigning like this
+var n1;
+n1 = 10;
+n1 = 'rajan';
+//but not ok if you are assigning like this
+var n2 = 10;
+//n2 = 'str'
 var result = add(n1, n2);
 console.log(result);
--- Sample/main.ts
+++ Sample/main.ts
@@ -1,7 +1,7 @@
 // var input1:Element | null = document.querySelector('#number1') as HTMLInputElement;
 // var input2:Element | null = document.querySelector('#number2') as HTMLIFrameElement;
 // var button = document.querySelector('#button');
-// const add = (number1:number,number2:number)=>{
+// const addition = (number1:number,number2:number)=>{
 //     return number1+number2
 // }
 // button?.addEventListener('click',()=> {
@@ -11,8 +11,15 @@
 const add = (number1:number,number2:number)=>{
     return number1+number2
 }
+//It is ok if you are assigning like this
+//Variable 'n1' implicitly has an 'any' type,
+// but a better type may be inferred from usage.ts(7043)
+let n1;
+n1 = 10;
+n1 = 'rajan';
 
-const n1=1.3;
-const n2=4;
-const result = add(n1,n2);
+//but not ok if you are assigning like this
+let n2 = 10;
+//n2 = 'str'
+let result = add(n1,n2);
 console.log(result);
